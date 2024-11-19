2a2220c919787581c7a58b17882300156de5ee86
Giuseppe <giuseppedibenedetto654@gmail.com>
2024-11-19 13:01:02 +0100

    Creazione palette e applicazione background

---
+++ The-Dandy-House/src/theme/colors.jsx
@@ -0,0 +1,44 @@
+const colors = {
+  background: "#121212",
+  text: {
+    primary: "#FFFFFF",
+    secondary: "#cacaca",
+    hover: {
+      text: "#C51400",
+    },
+    active: {
+      text: "#C51400",
+    },
+  },
+  status: {
+    succes: "#0edf65",
+    warning: "#db920b",
+    error: "#cf2714",
+  },
+  primaryButtons: {
+    background: "#770c00",
+    text: "#cacaca",
+    hover: {
+      background: "#911102",
+      text: "#FFFFFF",
+    },
+    active: {
+      background: "#911102",
+      text: "#FFFFFF",
+    },
+  },
+  secondaryButtons: {
+    background: "#eeeeee",
+    text: "#000000",
+    hover: {
+      background: "#000000",
+      text: "#FFFFFF",
+    },
+    active: {
+      background: "#000000",
+      text: "#FFFFFF",
+    },
+  },
+};
+
+export default colors;
